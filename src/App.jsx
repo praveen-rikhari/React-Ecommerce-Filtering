3ab4c070cbf86338c05b77da3ef1e651c0f40511
praveen-rikhari <praveenrikhari45@gmail.com>
2024-02-03 20:12:45 +0530

    Added filtering functionality

--- src/App.jsx
+++ src/App.jsx
@@ -12,13 +12,13 @@ function App() {
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [query, setQuery] = useState("");
 
-  const filteredItems = products.filter(
-    (product) => product.title.toLowerCase().indexOf(query.toLowerCase()) !== -1
-  );
-
   const handleInputChange = (e) => {
     setQuery(e.target.value);
   };
+
+  const filteredItems = products.filter(
+    (product) => product.title.toLowerCase().indexOf(query.toLowerCase()) !== -1
+  );
 
   const handleChange = (e) => {
     setSelectedCategory(e.target.value);
@@ -28,17 +28,51 @@ function App() {
     setSelectedCategory(e.target.value);
   };
 
+  function filteredData(products, selected, query) {
+    let filteredProducts = products;
 
+    // Filtering Input Items
+    if (query) {
+      filteredProducts = filteredItems;
+    }
 
+    // Applying selected filter
+    if (selected) {
+      filteredProducts = filteredProducts.filter(
+        ({ category, color, company, newPrice, title }) =>
+          category === selected ||
+          color === selected ||
+          company === selected ||
+          newPrice === selected ||
+          title === selected
+      );
+    }
+
+    return filteredProducts.map(
+      ({ img, title, star, reviews, prevPrice, newPrice }) => (
+        <Card
+          key={Math.random()}
+          img={img}
+          title={title}
+          star={star}
+          reviews={reviews}
+          prevPrice={prevPrice}
+          newPrice={newPrice}
+        />
+      )
+    );
+  }
+
+  const result = filteredData(products, selectedCategory, query);
 
   return (
     <>
       <Sidebar handleChange={handleChange} />
       <Nav query={query} handleInputChange={handleInputChange} />
       <Recommended handleClick={handleClick} />
-      <Products />
+      <Products result={result} />
     </>
   )
 }
 
-export default App
+export default App;
