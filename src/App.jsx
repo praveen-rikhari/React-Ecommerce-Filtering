557453d7f3f496a33dfdf51a316a3a488f5f7519
praveen-rikhari <praveenrikhari45@gmail.com>
2024-02-03 18:37:55 +0530

    Created Recommended Bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,31 @@
 import React, { useState } from 'react'
 import Nav from './Navigation/Nav';
 import Sidebar from './Sidebar/SideBar';
+import Recommended from './Recommended/Recommended';
+import "./App.css"
 
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [query, setQuery] = useState("");
 
-  const handleInputChange = (event) => {
-    setQuery(event.target.value);
+  const handleInputChange = (e) => {
+    setQuery(e.target.value);
   };
 
-  const handleChange = (event) => {
-    setSelectedCategory(event.target.value);
+  const handleChange = (e) => {
+    setSelectedCategory(e.target.value);
   };
 
+  const handleClick = (e) => {
+    setSelectedCategory(e.target.value);
+  };
 
   return (
     <>
       <Sidebar handleChange={handleChange} />
       <Nav query={query} handleInputChange={handleInputChange} />
+      <Recommended handleClick={handleClick} />
     </>
   )
 }
