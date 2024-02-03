6358f2c7a6f976676add9495bc8bcd44eee566a0
praveen-rikhari <praveenrikhari45@gmail.com>
2024-02-03 18:30:57 +0530

    Created Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import React, { useState } from 'react'
 import Nav from './Navigation/Nav';
+import Sidebar from './Sidebar/SideBar';
+
 
 function App() {
-
+  const [selectedCategory, setSelectedCategory] = useState(null);
   const [query, setQuery] = useState("");
 
   const handleInputChange = (event) => {
     setQuery(event.target.value);
   };
 
+  const handleChange = (event) => {
+    setSelectedCategory(event.target.value);
+  };
+
+
   return (
     <>
+      <Sidebar handleChange={handleChange} />
       <Nav query={query} handleInputChange={handleInputChange} />
     </>
   )
