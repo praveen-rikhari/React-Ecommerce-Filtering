9bf46a81352ba9e583457249abe6210349840bcb
praveen-rikhari <praveenrikhari45@gmail.com>
2024-02-03 18:54:19 +0530

    Added Products

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@ import React, { useState } from 'react'
 import Nav from './Navigation/Nav';
 import Sidebar from './Sidebar/SideBar';
 import Recommended from './Recommended/Recommended';
+import products from "./storeDB/storeDb";
+import Card from "./Components/Card";
 import "./App.css"
+import Products from './Products/Product';
 
 
 function App() {
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [query, setQuery] = useState("");
+
+  const filteredItems = products.filter(
+    (product) => product.title.toLowerCase().indexOf(query.toLowerCase()) !== -1
+  );
 
   const handleInputChange = (e) => {
     setQuery(e.target.value);
@@ -21,11 +28,15 @@ function App() {
     setSelectedCategory(e.target.value);
   };
 
+
+
+
   return (
     <>
       <Sidebar handleChange={handleChange} />
       <Nav query={query} handleInputChange={handleInputChange} />
       <Recommended handleClick={handleClick} />
+      <Products />
     </>
   )
 }
